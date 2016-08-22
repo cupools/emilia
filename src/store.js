e972e1266e88961a1dd194a630adad42130e955b
cupools <cupools@gmail.com>
2016-08-22 23:37:35 +0800

    update store

--- src/store.js
+++ src/store.js
@@ -1,23 +1,25 @@
 let Cache = {}
-let Storage = {
-    storage: {
-        style: {},
-        image: {},
-        sprite: {}
-    },
+class Storage {
+    constructor() {
+        this.storage = {
+            style: {},
+            image: {},
+            sprite: {}
+        }
+    }
     add(file, unique = 'realpath') {
         let t = file.type.toLowerCase()
         return this.storage[t] && (this.storage[t][file[unique]] = file)
-    },
+    }
     get(unique) {
         return this.storage.sprite[unique] || this.storage.style[unique] || this.storage.image[unique] || null
-    },
+    }
     get styles() {
         return this.storage.style
-    },
+    }
     get images() {
         return this.storage.image
-    },
+    }
     get sprites() {
         return this.storage.sprite
     }
@@ -25,7 +27,7 @@ let Storage = {
 
 export default {
     create() {
-        return Object.assign({}, Storage)
+        return new Storage()
     },
     cache(file, unique = 'realpath') {
         Cache[file[unique]] = file
