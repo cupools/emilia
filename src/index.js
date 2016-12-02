7ef9fa53d2f7ddb2d0d8065e94f134eee027f026
cupools <cupools@gmail.com>
2016-12-02 23:52:28 +0800

    refactor: build sprite

--- src/index.js
+++ src/index.js
@@ -10,10 +10,18 @@ export default function emilia(opts = {}, content) {
   const assignTag = item => ({ ...item, ...urlDetect(item.url) }) // => [{ url, decl, tag, seperator }, ...]
   const assignPath = item => ({ ...item, filepath: resolveUrl(item.url) })
   const assignBuffer = item => ({ ...item, buffer: getBuffer(item.filepath) })
-  const maps = raw.map(map(assignBuffer, assignPath, assignTag))
+  const bundles = raw.map(map(assignBuffer, assignPath, assignTag))
 
   const { algorithm, padding } = options
   const builder = sprite.bind(null, { algorithm, padding })
+  const tags = bundles.reduce(
+    (ret, { tag }) => (Array.includes(ret, tag) ? ret : ret.concat(tag)),
+    []
+  )
+
+  const outputs = tags
+    .map(tag => builder(bundles.filter(item => item.tag === tag)
+    .map(item => item.buffer)))
 }
 
 function map(...fns) {
