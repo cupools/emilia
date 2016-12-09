05bccbe2b06ab93c0e3900095947359ab593ddff
cupools <cupools@gmail.com>
2016-12-09 22:00:59 +0800

    feat: candy function now works

--- src/candy.js
+++ src/candy.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
+import path from 'path'
 import proof from 'proof'
+import mkdirp from 'mkdirp'
 import emilia from './index'
 
 const lints = {
@@ -22,5 +24,25 @@ export default function candy(opts) {
   const { src, dest, output, ...options } = proof(opts, lints)
   const { content, items } = emilia(options, fs.readFileSync(src, 'utf8'))
 
-  console.log(content)
+  writeFile(content, 'utf8', dest)
+  items.forEach(item => {
+    const { id, group, sprite, tag } = item
+    if (group.indexOf(id) === 0) {
+      writeFile(sprite.buffer, 'base64', output, tag + '.png')
+    }
+  })
+
+  return {
+    content,
+    items
+  }
+}
+
+function writeFile(content, encoding, ...p) {
+  const output = path.join(...p)
+  mkdirp.sync(
+    path.dirname(output.replace(/\/\w+?\.\w+$/, '') + '/__')
+  )
+
+  return fs.writeFileSync(output, content, encoding)
 }
--- test/candy.test.js
+++ test/candy.test.js
@@ -1,15 +1,20 @@
 /* eslint-env mocha */
 
 import path from 'path'
+import del from 'del'
 import candy from '../src/candy'
 import './common'
 
 describe('candy', function () {
+  beforeEach(function () {
+    del.sync('test/tmp')
+  })
+
   it('should work', function () {
     const options = {
       src: 'test/fixtures/css/custom.css',
-      dest: 'test/fixtures/tmp',
-      output: 'test/fixtures/tmp',
+      dest: 'test/tmp/output.css',
+      output: 'test/tmp/img/',
       resolveUrl: url => path.join(__dirname, 'fixtures/css', url)
     }
     candy(options)
